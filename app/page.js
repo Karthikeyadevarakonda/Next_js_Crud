e49e4669fd5ad57626fc6b54a7e19b972b430fd6
KarthikeyaDevarakonda <karthikeya8787@gmail.com>
2025-05-30 20:45:55 +0530

    deploy newly

--- app/page.js
+++ app/page.js
@@ -8,40 +8,41 @@ import { useEffect, useState } from "react"
 import { BASE_URL } from "@/constants/constants";
 
 const Page = () => {
-  if(!process.env.NEXT_PUBLIC_BASE_URL){
-    return null
-  }
-    const [data,setData] = useState([])
-    async function FetchData() {
-       try{
-          const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/UserData`); 
-        if(res?.data?.data){
-            setData(res.data.data)
-        }
-       }catch(err){
-        console.error("ERROR IN DATA FETCH",err)
-       }
-    }
-        useEffect(()=>{
-          FetchData()
-        },[])
-  return (
+  const [data, setData] = useState([])
 
+  useEffect(() => {
+    async function FetchData() {
+      try {
+        const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/UserData`)
+        if (res?.data?.data) {
+          setData(res.data.data)
+        }
+      } catch (err) {
+        console.error("ERROR IN DATA FETCH", err)
+      }
+    }
+
+    if (process.env.NEXT_PUBLIC_BASE_URL) {
+      FetchData()
+    }
+  }, [])
+
+  if (!process.env.NEXT_PUBLIC_BASE_URL) {
+    return <div>Missing environment variable: NEXT_PUBLIC_BASE_URL</div>
+  }
+
+  return (
     <div className="flex flex-col gap-5 px-4 py-8">
-     {data?.map((obj)=>{
-      return(
-    <div key={obj._id}  className="border px-5 py-4 ">
-        <h1 className="font-bold text-xl">{obj.name.toUpperCase()}</h1>
-        <h3>{obj.description}</h3>
-        <div className="flex justify-end gap-5">
-          <DeleteButton id={obj._id} setData={setData}/>
-          <Link href={`/update/${obj._id}`}><HiPencilSquare size={24} /></Link>
+      {data?.map((obj) => (
+        <div key={obj._id} className="border px-5 py-4">
+          <h1 className="font-bold text-xl">{obj.name.toUpperCase()}</h1>
+          <h3>{obj.description}</h3>
+          <div className="flex justify-end gap-5">
+            <DeleteButton id={obj._id} setData={setData} />
+            <Link href={`/update/${obj._id}`}><HiPencilSquare size={24} /></Link>
+          </div>
         </div>
-      </div>
-      )
-     })}
-       
-       
+      ))}
     </div>
   )
 }
